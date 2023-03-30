656b298ea97277540be495cbadea46e75efebba0
AjAutomation <AjAutomation09@gmail.com>
2023-03-30 13:10:05 +0530

    Revert "My 2nd commit"
    This reverts commit 75012a73d36aaa7b8d2e050b9a93cce51c626f6e.
    Reverting this changes

--- src/test/java/tests/IntelliJToGithub.java
+++ src/test/java/tests/IntelliJToGithub.java
@@ -3,7 +3,5 @@ package tests;
 public class IntelliJToGithub {
     public static void main(String[] args) {
         System.out.println("Share project on GitHub");
-		
-		//New changes to this Java code
     }
 }
