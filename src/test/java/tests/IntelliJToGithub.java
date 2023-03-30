75012a73d36aaa7b8d2e050b9a93cce51c626f6e
AjAutomation <AjAutomation09@gmail.com>
2023-03-30 13:07:02 +0530

    My 2nd commit

--- src/test/java/tests/IntelliJToGithub.java
+++ src/test/java/tests/IntelliJToGithub.java
@@ -3,5 +3,7 @@ package tests;
 public class IntelliJToGithub {
     public static void main(String[] args) {
         System.out.println("Share project on GitHub");
+		
+		//New changes to this Java code
     }
 }
